6636ce61af619d9c0e9f32c9ab0c233a9c5ab22a
Michael <michael.elliston95@gmail.com>
2025-12-12 10:54:00 -0600

    Initial commit

--- frontend-ui/js/application.js
+++ frontend-ui/js/application.js
@@ -109,7 +109,7 @@ function setMaxPrice(control)
     const label = document.getElementById("max-price-display")
     label.innerText = control.value;
 
-    const value = control.value != 1500 ? control.value : "";
+    const value = control.value != 200 ? control.value : "";
     productService.addMaxPriceFilter(value)
     productService.search();
 
