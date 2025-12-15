ec664202fd4e6b42be3657a80655b20f533a4a52
michaelelliston <michael.elliston95@gmail.com>
2025-12-15 14:19:26 -0600

    Implemented annotations and code for methods required for MVP.

--- backend-api/src/main/java/org/yearup/controllers/CategoriesController.java
+++ backend-api/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,6 +1,7 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
@@ -41,35 +42,38 @@ public class CategoriesController
         return this.categoryDao.getById(id);
     }
 
-    // the url to return all products in category 1 would look like this
-    // https://localhost:8080/categories/1/products
-    @GetMapping("{categoryId}/products")
+    // Runs when requesting a Get at the mapped path, and passes in the id from the path url.
+    @RequestMapping(path = "{categoryId}/products", method = RequestMethod.GET)
     public List<Product> getProductsById(@PathVariable int categoryId)
     {
-        // get a list of product by categoryId
-        return null;
+        return this.productDao.listByCategoryId(categoryId);
     }
 
-    // add annotation to call this method for a POST action
-    // add annotation to ensure that only an ADMIN can call this function
+    // Runs when requesting a Post at the mapped path, and passes the request's body in as a Category object.
+    // Requires the user to be an admin.
+    @RequestMapping(path = "/categories", method = RequestMethod.POST)
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public Category addCategory(@RequestBody Category category)
     {
-        // insert the category
-        return null;
+        return this.categoryDao.create(category);
     }
 
-    // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
-    // add annotation to ensure that only an ADMIN can call this function
+    // Runs when requesting a Put at the mapped path, and passes the path variable and request body into the method.
+    // Requires the user to be an admin.
+    @RequestMapping(path = "/categories/{id}", method = RequestMethod.PUT)
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void updateCategory(@PathVariable int id, @RequestBody Category category)
     {
-        // update the category by id
+        this.categoryDao.update(id, category);
     }
 
 
     // add annotation to call this method for a DELETE action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
+    @RequestMapping(path = "/categories/{id}", method = RequestMethod.DELETE)
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void deleteCategory(@PathVariable int id)
     {
-        // delete the category by id
+        this.categoryDao.delete(id);
     }
 }
