1dc64e5b9664d8da60b57a5205bf6142bcbf699d
michaelelliston <michael.elliston95@gmail.com>
2025-12-16 14:49:21 -0600

    Modified annotations and parameters

--- backend-api/src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ backend-api/src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -33,8 +33,8 @@ public class ShoppingCartController {
     }
 
     // each method in this controller requires a Principal object as a parameter
-    @GetMapping("")
-    public ShoppingCart getCart(@AuthenticationPrincipal Principal principal) {
+    @GetMapping()
+    public ShoppingCart getCart(Principal principal) {
         try {
             // get the currently logged in username
             String userName = principal.getName();
@@ -42,18 +42,15 @@ public class ShoppingCartController {
             User user = userDao.getByUserName(userName);
             int userId = user.getId();
 
-
             return this.shoppingCartDao.getByUserId(userId);
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
         }
     }
 
-// add a POST method to add a product to the cart - the url should be
-// https://localhost:8080/cart/products/15 (15 is the productId to be added
     @PostMapping("products/{productId}")
     @ResponseStatus(value = HttpStatus.CREATED)
-    public void addToCart(@AuthenticationPrincipal Principal principal, @PathVariable int productId) {
+    public void addToCart(Principal principal, @PathVariable int productId) {
 
         String userName = principal.getName();
         User user = userDao.getByUserName(userName);
